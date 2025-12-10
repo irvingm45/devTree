f87f3cbf061c214e874fb52edbd8eaae6b53c17f
Irvin <96852240+irvingm45@users.noreply.github.com>
2025-12-09 23:19:44 -0600

    Added a new script in package.json called 'nodemon' which can let run the project in watch mode

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Hola mundo desde Node');
+// const express = require('express');  // Sintaxis CommonJS
+import express from 'express'; // Sintaxis ES Modules
+
+const app = express();
+
+// cuando se despliega en un servidor real, muchas veces el puerto lo asigna el mismo servidor
+// process.env.PORT nos da el puerto asignado por el servidor
+const port = process.env.PORT || 4000;
+
+// Routing
+app.get('/', (req, res) => {
+	res.send('Hola mundo en Express');
+});
+
+app.get('/ecommerce', (req, res) => {
+	res.send('Bienvenido a la tienda en línea');
+})
+
+app.listen(port, () => {
+	console.log(`Servidor inciado en el puerto: ${port}`);
+});
