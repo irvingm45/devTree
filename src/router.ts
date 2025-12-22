a9b3739c08c5735955fa85014f18332e05d87870
Irvin <96852240+irvingm45@users.noreply.github.com>
2025-12-21 22:08:50 -0600

    Added messages if the fields are empty on the http response

--- src/router.ts
+++ src/router.ts
@@ -7,7 +7,15 @@ const router = Router();
 // Authentication and registration routes
 // Instead of using 'get', we use 'post' for better security
 router.post('/auth/register', 
-    body('handle').notEmpty(),
+    body('name')
+        .notEmpty()
+        .withMessage('Name is required'),
+    body('handle')
+        .notEmpty()
+        .withMessage('Handle is required'),
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email address'),
     createAccount);
 
 export default router;
